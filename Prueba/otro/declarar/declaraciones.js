f793d97f3fb8ae20af313daaa3e2c2d09db908d5
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2022-10-11 14:22:39 -0500

    Tarea 2.1.1

---
+++ Prueba/otro/declarar/declaraciones.js
@@ -0,0 +1,23 @@
+if (true){
+    let saludo = "Hola";
+}
+console.log(saludo);
+
+function Hola(){
+    let saludo = "Hola";
+}
+Hola();
+console.log(saludo);
+
+function Hola(){
+    saludo = "Hola";
+}
+Hola();
+console.log(saludo);
+
+function saludar(){
+    frase = "hola";
+    console.log(frase)
+    var frase;
+}
+saludar();
