5cb5ec384e592449b55bd062585df54192d4f976
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2022-10-11 18:39:24 -0500

    Tarea 2.1.1

---
+++ Parcial2/RegistroEventos/script.js
@@ -0,0 +1,6 @@
+document.getElementById("miBoton2").onclick = function(){
+    alert("Click en boton 2");
+};
+document.getElementById("miBoton3").addEventListener("click",function(){
+    alert("Boton numero 3");
+});
