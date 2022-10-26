2be7066016a6dd7eb347362e8fb6e680266a1941
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2022-10-26 14:22:11 -0500

    b

--- Parcial2/XmlHttp/sidebar.js
+++ Parcial2/XmlHttp/sidebar.js
@@ -1,7 +1,15 @@
-
-
 $(document).ready(function() {
     $('#sidebar-btn').on('click', function() {
         $('#sidebar').toggleClass('visible');
     });
+
+    $('#botonjax').click(presionButon)
+
+    function presionButon(){
+        var puram = $("#num").val();
+    
+        $.get("cargaCuadrado.php",(num: puram), llegadaDatos)
+        //return false
+    }
 });
+
