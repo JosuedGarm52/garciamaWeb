b009af1a09214aa7b11c1c4a71de83a1c636e1cd
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2022-11-24 01:01:27 -0600

    Tarea 3.7.1 Modal para llenado

--- Parcial3/FormData/src/consultaDB2.php
+++ Parcial3/FormData/src/consultaDB2.php
@@ -1,9 +1,6 @@
 <?php
-    $name = $_POST['exampleInputNombre'];
-    /*if(isset($_POST['Nombre']))
-    {
-        $id = $_POST['Nombre'];
-    }*/
+    $id = $_POST['message_text'];
+    
     
 
     $servidor = "localhost";
@@ -12,7 +9,7 @@
     $password = "";
     //echo 'select * from datos where ID='."\n";
     $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar a localhost");
-    $consulta = "select * from datos where Nombre = '$name'";
+    $consulta = "select * from datos where id = '$id'";
     //echo $consulta."\n";
     $registros = mysqli_query($con,$consulta) or die("Problemas con el select");
     
--- Parcial3/FormData/src/index.js
+++ Parcial3/FormData/src/index.js
@@ -19,8 +19,9 @@
             document.getElementById("InputUser").value=dato.Username;
             document.getElementById("exampleInputEmail1").value=dato.Correo;
             document.getElementById("exampleInputEmail2").value=dato.Correo;
-            document.getElementById("exampleInputPassword1").value=dato.Contraseña;
-            document.getElementById("exampleInputPassword2").value=dato.Contraseña;
+            document.getElementById("exampleInputPassword1").value=dato.Contrasena;
+            document.getElementById("exampleInputPassword2").value=dato.Contrasena;
+            
         }else
         {
             console.log("No has introducido un nombre :(");
@@ -44,3 +45,55 @@
         document.getElementById('Cuerpo').innerHtml = ``+mensaje;
         x.show();
     };
+    $("#btnQuery").click( function (){
+        $("modal").modal("show");
+    })
+    
+    $("#cerrar, #modalsave").click( function (){
+        $("modal").modal("show");
+    })
+
+    const exampleModal = document.getElementById('exampleModal')
+    exampleModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        const recipient = button.getAttribute('data-bs-whatever')
+        // If necessary, you could initiate an AJAX request here
+        // and then do the updating in a callback.
+        //
+        // Update the modal's content.
+        const modalTitle = exampleModal.querySelector('.modal-title')
+        const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+        
+    })
+
+    document.getElementById("btnServer").addEventListener('click',async ()=>{
+        let name = message_text.value;
+        if(NoNuloOVacio(name)){
+            //console.log("Hola")
+            var datoformula1 = new FormData(document.getElementById('modalid'));
+            let respuesta  = await fetch('consultaDB2.php',{
+                method: 'POST',
+                body: datoformula1
+            })
+            
+            let dato = await respuesta.json();
+            
+            //console.log(dato);
+            document.getElementById("exampleInputNombre").value=dato.Nombre;
+            document.getElementById("exampleInputApellido").value=dato.Apellido;
+            document.getElementById("InputUser").value=dato.Username;
+            document.getElementById("exampleInputEmail1").value=dato.Correo;
+            document.getElementById("exampleInputEmail2").value=dato.Correo;
+            document.getElementById("exampleInputPassword1").value=dato.Contrasena;
+            document.getElementById("exampleInputPassword2").value=dato.Contrasena;
+            
+        }else
+        {
+            console.log("No has introducido un ID :(");
+            //AbrirModal("Error","Aun no introduces un valor en el nombre para el autollenado")
+        }
+        
+    }); 
